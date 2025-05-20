ed95bac8748d03d4d397cf1e3870501f11d8febc
waik1ki <arbutus@outlook.kr>
2025-05-20 09:21:22 +0900

    feat: GlobalPipe 적용

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  await app.listen(process.env.port ?? 3001);
 }
 bootstrap();

68843751636cedf0e775981bd958f6d6d44cf9be
waik1ki <arbutus@outlook.kr>
2025-05-20 09:18:34 +0900

    feat: gateway 로직 추가

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
+import { JwtModule } from '../../../libs/jwt/src';
+import { RolesGuard } from './guards/roles.guard';
+import { EventController } from './event-proxy.controller';
+import { JwtAuthGuard } from 'apps/gateway/src/guards/jwt-auth.guard';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { GatewayService } from './gateway.service';
+import { AuthController } from './auth-proxy.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { CacheModule } from './cache/cache.module';
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.forRootAsync(),
+    HttpModule,
+    CacheModule,
+  ],
+  controllers: [AuthController, EventController],
+  providers: [
+    GatewayService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
+  exports: [GatewayService],
 })
 export class GatewayModule {}
--- apps/gateway/src/gateway.service.ts
+++ apps/gateway/src/gateway.service.ts
@@ -1,8 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { catchError, map, Observable } from 'rxjs';
+import { Request } from 'express';
+import { AxiosRequestConfig } from 'axios';
+import { CacheService } from './cache/cache.service';
 
 @Injectable()
 export class GatewayService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+    private readonly cacheService: CacheService,
+  ) {}
+
+  async proxy(
+    service: string,
+    req: Request,
+  ): Promise<Observable<AxiosRequestConfig<any>>> {
+    const baseUrl = this.configService.get(`${service}_SERVICE_URL`);
+
+    const headers: Record<string, string> = {};
+    headers['x-user-id'] = req['user']?.userId;
+    headers['x-user-role'] = req['user']?.role;
+    headers['content-type'] = req.headers['content-type'] as string;
+
+    if (req.method === 'POST') {
+      const ok = await this.cacheService.checkAndSet(
+        req['user']?.userId || req.ip || req.host,
+        req.path,
+      );
+      if (!ok) {
+        throw new HttpException('Duplicate request', HttpStatus.CONFLICT);
+      }
+    }
+
+    return this.httpService
+      .request({
+        headers,
+        method: req.method,
+        url: baseUrl + req.path,
+        data: req.body,
+      })
+      .pipe(
+        map((resp) => resp.data),
+        catchError((err) => {
+          throw err.response
+            ? new HttpException(
+                err.response.data as string | Record<string, any>,
+                err.response.status as number,
+              )
+            : err;
+        }),
+      );
   }
 }

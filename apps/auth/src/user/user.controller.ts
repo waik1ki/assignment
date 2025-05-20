d2135f120446c577547d72161e424420dfa590dd
waik1ki <arbutus@outlook.kr>
2025-05-20 09:13:57 +0900

    feat: user 컨트롤러 추가

---
+++ apps/auth/src/user/user.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserResponseDto } from './dtos/user-response.dto';
+import { CreateUserRequestDto } from './dtos/create-user-request.dto';
+import { UpdateUserRequestDto } from './dtos/update-user-request.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // 유저 등록
+  @Post()
+  createUser(@Body() dto: CreateUserRequestDto): Promise<UserResponseDto> {
+    return this.userService.createUser(dto);
+  }
+
+  // 유저 수정
+  @Patch(':id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() dto: UpdateUserRequestDto,
+  ): Promise<UserResponseDto> {
+    return this.userService.updateUser(id, dto);
+  }
+}
